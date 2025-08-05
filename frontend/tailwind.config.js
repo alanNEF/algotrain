45e110f01c76f4c668973fa0c97211fd5a2bd868
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2025-08-05 11:09:18 -0400

    Front end set up

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+    content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+})
